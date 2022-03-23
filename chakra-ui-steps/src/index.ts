2654bd5349ad82319200e769a1c89987851baba1
rixcian <rostislav.kremecek@protonmail.com>
2022-03-23 15:16:13 +0100

    refactor: export components types

--- chakra-ui-steps/src/index.ts
+++ chakra-ui-steps/src/index.ts
@@ -2,3 +2,6 @@ export { Step } from './components/Step';
 export { Steps } from './components/Steps';
 export { useSteps } from './hooks/useSteps';
 export { StepsStyleConfig } from './theme';
+
+export type { StepProps } from './components/Step';
+export type { StepsProps } from './components/Steps';
