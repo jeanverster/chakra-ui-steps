455379518771fc8cbdeaa42e2adc0b10f508f371
Jean Verster <jeanverster0107@gmail.com>
2022-03-28 09:17:13 +0200

    1.7.2 (#62)
    * fix: add isCompletedStep to the step props (#48)
    
    This will fix the type 'IntrinsicAttributes & ObjectDto' problem on Step.
    
    Co-authored-by: Jean Verster <jeanverster0107@gmail.com>
    
    * chore(release): 1.6.2 [skip ci]
    
    ## [1.6.2](https://github.com/jeanverster/chakra-ui-steps/compare/v1.6.1...v1.6.2) (2022-02-14)
    
    ### Bug Fixes
    
    * add isCompletedStep to the step props ([#48](https://github.com/jeanverster/chakra-ui-steps/issues/48)) ([be22925](https://github.com/jeanverster/chakra-ui-steps/commit/be22925117a8f6c433b487c9af667950c7d31717))
    
    * feat: switch to vite (#54)
    
    * fix: update donation link
    
    * Release 1.6.2 (#49)
    
    * fix: add isCompletedStep to the step props (#48)
    
    This will fix the type 'IntrinsicAttributes & ObjectDto' problem on Step.
    
    Co-authored-by: Jean Verster <jeanverster0107@gmail.com>
    
    * chore(release): 1.6.2 [skip ci]
    
    ## [1.6.2](https://github.com/jeanverster/chakra-ui-steps/compare/v1.6.1...v1.6.2) (2022-02-14)
    
    ### Bug Fixes
    
    * add isCompletedStep to the step props ([#48](https://github.com/jeanverster/chakra-ui-steps/issues/48)) ([be22925](https://github.com/jeanverster/chakra-ui-steps/commit/be22925117a8f6c433b487c9af667950c7d31717))
    
    Co-authored-by: Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    
    * merge master
    
    * chore: configure yarn workspaces
    
    * feat: vitest over jest
    
    * chore: reference local package correctly
    
    * feat: setup gh actions
    
    * chore: fix indentation
    
    * chore: remove jest specific commands
    
    Co-authored-by: Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    
    * fix actions
    
    * trigger build
    
    * remove paths
    
    * update action config
    
    * set persist credentials to fasle
    
    * chore(release): 1.0.0 [skip ci]
    
    # chakra-ui-steps-v1.0.0 (2022-03-19)
    
    ### Features
    
    * switch to vite ([#54](https://github.com/jeanverster/chakra-ui-steps/issues/54)) ([2683cd5](https://github.com/jeanverster/chakra-ui-steps/commit/2683cd52363c46b98eded64256b39f6c6a5da8cc)), closes [#49](https://github.com/jeanverster/chakra-ui-steps/issues/49) [#48](https://github.com/jeanverster/chakra-ui-steps/issues/48) [#48](https://github.com/jeanverster/chakra-ui-steps/issues/48)
    
    * trigger ci
    
    * merge changes
    
    * refactor: export components types (#59)
    
    * Refactor/split step label (#60)
    
    * refactor: add option to style iconLabel separately
    
    * docs: add iconLabel to README
    
    Co-authored-by: Jean Verster <jeanverster0107@gmail.com>
    
    Co-authored-by: Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: rixcian <rostislav.kremecek@protonmail.com>

---
+++ chakra-ui-steps/vite.config.ts
@@ -0,0 +1,44 @@
+import typescript from '@rollup/plugin-typescript';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+const path = require('path');
+
+const isExternal = (id: string) => !id.startsWith('.') && !path.isAbsolute(id);
+const resolvePath = (str: string) => path.resolve(__dirname, str);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: isExternal,
+      output: {
+        globals: {
+          react: 'react',
+          'framer-motion': 'framer-motion',
+          '@chakra-ui/system': '@chakra-ui/system',
+          '@chakra-ui/theme-tools': '@chakra-ui/theme-tools',
+          '@chakra-ui/utils': '@chakra-ui/utils',
+          '@chakra-ui/icon': '@chakra-ui/icon',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          '@chakra-ui/react': '@chakra-ui/react',
+          '@chakra-ui/transition': '@chakra-ui/transition',
+        },
+      },
+      plugins: [
+        typescript({
+          target: 'ESNext',
+          rootDir: resolvePath('./src'),
+          declaration: true,
+          declarationDir: resolvePath('./dist'),
+          exclude: resolvePath('./node_modules/**'),
+          allowSyntheticDefaultImports: true,
+        }),
+      ],
+    },
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'chakra-ui-steps',
+    },
+  },
+});
