90ba23343395719fa11f8cfbd89d3025489075c6
Jean Verster <jeanverster0107@gmail.com>
2023-02-05 13:58:43 +0100

    chore: delete mdx files & config

--- website/mdx/components/P.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, TextProps } from "@chakra-ui/layout";
-
-export const P = (props: TextProps) => {
-  if (typeof props.children !== "string") {
-    return <>{props.children}</>;
-  }
-
-  return <Text mb={8} lineHeight="taller" {...props} />;
-};
--- website/sections/getting-started/GettingStarted.tsx
+++
@@ -1,5 +0,0 @@
-interface AppProps {}
-
-export const App = (props: AppProps) => {
-  return <div />;
-};
