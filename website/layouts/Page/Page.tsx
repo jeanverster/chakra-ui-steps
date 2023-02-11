6711557687a8b02c96edd68e8363376fda385f47
Jean Verster <jeanverster0107@gmail.com>
2023-02-11 17:32:18 +0100

    chore: add og image

--- website/layouts/Page/Page.tsx
+++ website/layouts/Page/Page.tsx
@@ -58,6 +58,10 @@ export const Page = ({
       <Head key="page">
         <title>{metaTitle}</title>
         <meta name="description" content={metaDescription} />
+        <meta
+          property="og:image"
+          content="https://chakra-ui-steps.vercel.app/api/og"
+        />
       </Head>
 
       {showBackButton && (
